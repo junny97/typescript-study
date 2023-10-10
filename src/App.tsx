673f751e6c9ccb00a566564f9793f70f6d150fc1
junny97 <pcohad12@gmail.com>
2023-10-10 22:39:43 +0900

    [typescript] event의 type 정의방법

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  return <div>APP</div>;
-}
+  const [value, setValue] = useState('');
+  //타입스크립트에서는 event의 type을 정의해줘야 한다
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    //it's same const value = event.currentTarget.value;
+    const {
+      //currentTarget = target
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
 
+  //onsubmit 의 event도 type 명시해주기
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('hello', value);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type=' text'
+          placeholder='username'
+        />
+        <button>Log in</button>
+      </form>
+    </>
+  );
+}
 export default App;
